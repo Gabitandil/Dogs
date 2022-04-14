0a3203dea850159ce4bc68604bda470676a8137d
Gabriel <pein4302@gmail.com>
2022-04-14 13:39:18 -0300

    css

--- client/src/components/paginado/paginado.jsx
+++ client/src/components/paginado/paginado.jsx
@@ -3,7 +3,7 @@ import style from '../paginado/paginado.module.css'
 export default function Paginado({ dogsPerPage, perros, paginado }) {
     const pageNumbers = []
 
-    for (let i = 0; i <= Math.ceil(perros / dogsPerPage); i++) {
+    for (let i = 1; i <= Math.ceil(perros / dogsPerPage); i++) {
         pageNumbers.push(i)
 
     }
@@ -12,15 +12,15 @@ export default function Paginado({ dogsPerPage, perros, paginado }) {
 
 
     return (
-        <div className={style.test}>
+        <div className={style.test} >
             <nav>
                
                 <ul>
                     {pageNumbers && pageNumbers.map((el) => {
-                        return (<li>
+                        return (<li className={style.paginado}  >
 
 
-                            <a onClick={() => paginado(el)}>{el}</a>
+                            <a  onClick={() => paginado(el)}>{el}</a>
                        
 
 
