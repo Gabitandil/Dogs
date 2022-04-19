a535b467953b9dc1a6bde7c614bf048f89276d97
Gabriel <pein4302@gmail.com>
2022-04-18 22:20:56 -0300

    push

--- client/src/components/details/details.jsx
+++ client/src/components/details/details.jsx
@@ -1,40 +1,51 @@
 import React, { useEffect } from 'react'
+import styles from '../details/details.module.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
 import { getDetails } from '../../redux/actions'
+import NavBar from '../navbar/navbar'
 export default function Details() {
- let {id} = useParams()
- const dogsDetails = useSelector(state => state.oneDog)
- const dispatch = useDispatch()
- useEffect(() => {
-   dispatch(getDetails(id))
- }, [dispatch])
- 
-console.log('holaa', dogsDetails.name)
-  return (
-    <div>
-        
-        {
-         dogsDetails? (
-             <div>
-                 
-                <p>{dogsDetails.name}</p>
-                <img src={dogsDetails.image} alt="" />
+    let { id } = useParams()
+    const dogsDetails = useSelector(state => state.oneDog)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getDetails(id))
+    }, [])
+
+
+    return (
+        <div >
+            <NavBar/>
+           
+            {
+                
+                Object.keys(dogsDetails).length>0 ? (
+                    <div className={styles.border}>
+
+                        <img className={styles.image} src={dogsDetails.image} alt="" />
+                        <h1>{dogsDetails.name}</h1>
+                        <h4>life span: {dogsDetails.years}</h4>
+                        <h4>height : {dogsDetails.height}</h4>
+                        <h4>weight:</h4>
+                        <h4>{dogsDetails.weight} kg </h4>
+
+                        <Link to= '/home'>
+                            <button>volver</button>
+                        </Link>
+                        
+                        
+                    </div>
+                    
+                ) : <div> <h5>loading...</h5></div>
+                    
+                
+                
 
 
 
-            </div>
-
-         ) 
-          : <p>loading</p>   
+            }
 
 
-
-
-
-        }
-        
-
-    </div>
-  )
+        </div>
+    )
 }
